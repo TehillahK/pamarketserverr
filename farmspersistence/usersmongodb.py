1c639d23c65926e1d9f17f97c0fa947dfb56e9eb
Tehilah <tkangamba@hotmail.com>
2022-07-02 03:37:49 -0400

    added message

--- farmspersistence/usersmongodb.py
+++ farmspersistence/usersmongodb.py
@@ -21,13 +21,17 @@ class UsersMongoDB(UserDatabase):
             user = col.find(my_query).limit(1)
             result = dumps(user[0])
             result = json.loads(result)
-            result["message"]="success"
+            result["message"] = "success"
             result = json.dumps(result)
+            my_client.close()
         except pymongo.errors.ConnectionFailure:
             print("failed to get user")
             result = None
         except Exception as e:
-            result=None
+            result = None
             print("failed")
             print(e)
         return result
+
+    def add_user(self):
+        pass
